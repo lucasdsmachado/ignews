b85e15c28cc38c7a114ee33099ce0e4da16d2b51
lucasdsmachado <brian.machado@ufba.br>
2022-01-10 10:37:59 -0300

    configure next auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head"
 import { SubscribeButton } from "../components/SubscribeButton"
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import { stripe } from "../services/stripe"
 import styles from "./home.module.scss"
 
@@ -36,7 +36,7 @@ export default function Home({ product } : HomeProps) {
     )
 }
 
-export const getServerSideProps : GetServerSideProps = async () => {
+export const getStaticProps : GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KEWcwAxGv6QkKdqf6jFnvVj');
 
   const product = {
@@ -50,7 +50,8 @@ export const getServerSideProps : GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    }, 
+    revalidate: 60 * 60 * 24 // 24 hours
   }
 }
 
