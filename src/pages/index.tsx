e1b92b564a9e67e351554ccc2aaab7db6b9c99c1
lucasdsmachado <brian.machado@ufba.br>
2022-01-19 07:57:14 -0300

    add post page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home({ product } : HomeProps) {
   return (
     <>
       <Head>
-        <title>Home | ig.news</title>
+        <title>Home | ignews</title>
       </Head>
       
       <main className={styles.contentContainer}>
