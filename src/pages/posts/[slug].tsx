864fdfa37572c82624eea842294527159974439c
lucasdsmachado <brian.machado@ufba.br>
2022-01-19 14:44:52 -0300

    validate active subscribtion

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -37,7 +37,16 @@ export default function Post({ post } : PostProps) {
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
   const session = await getSession({ req });
   const { slug } = params;
-  
+
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  } 
+
   const prismic = getPrismicClient(req);
   const response = await prismic.getByUID<any>('post', String(slug), {});
 
